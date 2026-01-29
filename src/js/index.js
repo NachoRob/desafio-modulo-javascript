e1fb365af0941fcef372b9c0c62ad0d688bcf0da
NachoRob <ignaciorob6@gmail.com>
2026-01-29 20:05:38 -0300

    Hacemos el primer, y tal vez el último commit del desafío

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+precio = 400000
+cantidad = 0;
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+// El total también debería empezar en 0 para que no se vea vacío
+document.querySelector(".valor-total").innerHTML = 0;
